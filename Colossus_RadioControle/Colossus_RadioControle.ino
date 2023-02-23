782d4b9dcd24cf48875b567925f040f32aa928aa
AURELIO <cyberwisk@gmail.com>
2023-02-23 12:15:19 -0300

    Update Colossus_RadioControle.ino
    Commit as 12h

--- Colossus_RadioControle/Colossus_RadioControle.ino
+++ Colossus_RadioControle/Colossus_RadioControle.ino
@@ -8,19 +8,27 @@
 
 RF24 radio(9, 10);
 
-struct MeuControleRemoto  {  
+struct ControleColossus  {  
   boolean Botao_0;
   boolean Botao_1;
   boolean Botao_2;
   boolean Botao_3;
-  boolean Botao_4;
+  //boolean Botao_4;
   byte Joystick_AY;
   byte Joystick_BX;
+  //byte Joystick_AX;
+  //byte Joystick_BY;
+  //byte Pot_01;
+  //byte Pot_02;
  };
 
-MeuControleRemoto ControleRemoto;
-#define SPEAKERPIN 4
-#int CHANNEL = 100;
+ControleColossus Controle;
+#define SPEAKERPIN 8
+#define LED_GREEM A2
+#define LED_RED A3
+
+//Canal do Transmissor
+int CHANNEL = 100;
 
 void setup() {
 
@@ -44,6 +52,10 @@ void setup() {
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(8, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
 
   beep();
 }
@@ -53,38 +65,45 @@ void loop() {
   readVcc(); 
 
   //Botões
-  ControleRemoto.Botao_0 = digitalRead(2);
-  ControleRemoto.Botao_1 = digitalRead(3);
-  ControleRemoto.Botao_2 = digitalRead(4);
-  ControleRemoto.Botao_3 = digitalRead(5);
+  Controle.Botao_0 = digitalRead(2);
+  Controle.Botao_1 = digitalRead(3);
+  Controle.Botao_2 = digitalRead(4);
+  Controle.Botao_3 = digitalRead(5);
+  Controle.Botao_4 = digitalRead(6);
   
   //JOYSTICKS
-  ControleRemoto.Joystick_AY = map (analogRead(A1), 0, 1023, 0, 255);  
-  ControleRemoto.Joystick_BX = map (analogRead(A0), 0, 1023, 0, 255);  
+  Controle.Joystick_AY = map (analogRead(A1), 0, 1023, 0, 255);  
+  Controle.Joystick_BX = map (analogRead(A0), 0, 1023, 0, 255);
+  //Controle.Joystick_AX = map (analogRead(A2), 0, 1023, 0, 255);  
+  //Controle.Joystick_BY = map (analogRead(A3), 0, 1023, 0, 255);
+  //Controle.Pot_01 = map (analogRead(A4), 0, 1023, 0, 255);  
+  //Controle.Pot_02 = map (analogRead(A5), 0, 1023, 0, 255);     
 
   // Transmitindo dados
 
   for (int i = 0; i < 10; i++) {
-    radio.write( &ControleRemoto, sizeof(ControleRemoto) );
+    radio.write( &Controle, sizeof(Controle) );
+    digitalWrite(LED_GREEM, LOW);    
   }
 
+  // Mosta o resultado na Serial
   Serial.print("       Joystick_AY: ");
-  Serial.println(ControleRemoto.Joystick_AY);      
+  Serial.println(Controle.Joystick_AY);      
 
   Serial.print("       Joystick_BX: ");
-  Serial.println(ControleRemoto.Joystick_BX);     
+  Serial.println(Controle.Joystick_BX);     
 
   Serial.print("           Botao_0: ");
-  Serial.println(ControleRemoto.Botao_0);      
+  Serial.println(Controle.Botao_0);      
 
   Serial.print("           Botao_1: ");
-  Serial.println(ControleRemoto.Botao_1);      
+  Serial.println(Controle.Botao_1);      
 
   Serial.print("           Botao_2: ");
-  Serial.println(ControleRemoto.Botao_2);     
+  Serial.println(Controle.Botao_2);     
 
   Serial.print("           Botao_3: ");
-  Serial.println(ControleRemoto.Botao_3);      
+  Serial.println(Controle.Botao_3);      
 }
 
 void readVcc()
@@ -92,7 +111,7 @@ void readVcc()
   // Ler referência interna 1.1V
   // Ajusta a referência ao Vcc e a medição de referência interna para 1.1V
   ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Aguarde
+  delay(2);
   ADCSRA |= _BV(ADSC); // Inicia a conversão
   while (bit_is_set(ADCSRA, ADSC)); // Medindo
   uint8_t low = ADCL; // Vai ler ADCL primeiro - Então trava ADCH
@@ -103,26 +122,31 @@ void readVcc()
   Serial.print("           Bateria: ");
   Serial.println(result);    
   
-  if (result < 3100) { // Aciona o buzzer se a tensão da bateria estiver menor que 3,1 volts (para indicar que a bateria esta fraca)
+  if (result < 3100) { // Aciona o buzzer se a tensão da bateria estiver menor que 3,1 volts
+    digitalWrite(LED_GREEM, LOW);
     tone(SPEAKERPIN, 900, 100);
+    digitalWrite(LED_RED, HIGH);    
   }
+  digitalWrite(LED_RED, LOW);      
 }
 
 void beep()
 {
       int k = random(1000,2000);
     for (int i = 0; i <=  random(100,2000); i++){
-        
+        digitalWrite(LED_GREEM, LOW);
         tone(SPEAKERPIN, k+(-i*2));          
-        delay(random(.9,2));             
+        delay(random(.9,2));
+        digitalWrite(LED_RED, HIGH);             
     } 
     for (int i = 0; i <= random(100,1000); i++){
-        
+        digitalWrite(LED_GREEM, HIGH);
         tone(SPEAKERPIN, k + (i * 10));          
-        delay(random(.9,2));             
+        delay(random(.9,2));
+        digitalWrite(LED_RED, LOW);             
     }
-    tone (SPEAKERPIN,300,100); 
+    tone (SPEAKERPIN,300,100);
+    digitalWrite(LED_RED, HIGH); 
 }
-
 
 // FIM
